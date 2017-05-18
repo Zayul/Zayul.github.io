4c5d8709d59ab760d242efe6441f1c6c0387000c
Zale <zalerasco@gmail.com>
2017-05-18 14:28:41 -0400

    Tab Selection Update

--- scripts.js
+++ scripts.js
@@ -1,16 +1,16 @@
-function openTab(call, tabName) {
+function openTab(event, tabName) {
     var i;
 
     tabContent = document.getElementsByClassName("tabContent");
     for (i = 0; i < tabContent.length; i++) {
-        tabContent[i].style.display = "none"; 
+        tabContent[i].style.display = "none";
     }
 
     tabButton = document.getElementsByClassName("tabButton")
     for (i = 0; i < document.getElementsByClassName("tabButton").length; i++) {
-    	tabButton[i].className = tabButton[i].className.replace(" active", "")
+    	tabButton[i].className = tabButton[i].className.replace(" active", " inact")
     }
 
-    call.currentTarget.className += " active";
+    event.currentTarget.className = event.currentTarget.className.replace(" inact", " active");
     document.getElementById(tabName).style.display = "block"; 
 }
