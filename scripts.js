3269421033a0aca78448e18747d0bf56eacca37f
Zale <zalerasco@gmail.com>
2017-06-01 08:44:39 -0400

    Big update

--- scripts.js
+++ scripts.js
@@ -1,4 +1,24 @@
 var money = 0;
+var iq = 0;
+var d = new Date();
+var prev = d.getTime()-1;
+var now = d.getTime();
+var currentTask = "none";
+
+function delTime() {
+    d = new Date();
+    prev = now;
+    now = d.getTime();
+    var elapse = now - prev;
+    gameLoop(elapse);
+}
+
+function gameLoop(elapse) {
+    money += elapse;
+    iq += elapse;
+    //document.getElementById("iqCounter").innerHTML = "IQ " + iq;
+    document.getElementById("moneyCounter").innerHTML = "Counter: " + money;
+}
 
 function openTab(event, tabName) {
     var i;
@@ -10,17 +30,9 @@ function openTab(event, tabName) {
 
     tabButton = document.getElementsByClassName("tabButton")
     for (i = 0; i < document.getElementsByClassName("tabButton").length; i++) {
-    	tabButton[i].className = tabButton[i].className.replace(" active", " inActive")
+        tabButton[i].className = tabButton[i].className.replace(" active", " inActive")
     }
 
     event.currentTarget.className = event.currentTarget.className.replace(" inActive", " active");
-    document.getElementById(tabName).style.display = "block"; 
-}
-
-function gameLoop() {
-
-	money += 1;
-
-	document.getElementById("moneyCounter").innerHTML = money;
-	console.log("Done");
+    document.getElementById(tabName).style.display = "block";
 }
